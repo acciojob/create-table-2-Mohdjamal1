2521a9607978ec94909c8bc580a7ca672a3a890c
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-05-19 19:56:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function createTable() {
     //Write your code here
-  
+	let myTable = document.getElementById('myTable');
+	const row = parseInt(prompt('Input number of rows'));
+	let col = parseInt(prompt('Input number of columns'));
+
+	for (let i = 0; i < row; i++){
+		let tr = document.createElement('tr');
+		for (let j = 0; j < col; j++) {
+			let td = document.createElement('td');
+			td.innerText = `Row-${i} Column-${j}`;
+			tr.append(td);
+		}
+		myTable.append(tr);
+	}
 }
